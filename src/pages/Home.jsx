61baaea9a33a6b6f774eea5908797c2ed210a9c7
Gabriela Costa <gabrielasilvaalvescosta@gmail.com>
2022-01-08 21:36:51 -0300

    Slideshow (imagem)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Slideshow from '../components/Slideshow/Slideshow';
 
 const fotos = [
-  '../assets/slideshow.jpg',
+  'https://raw.githubusercontent.com/gabrielaalvescosta/livraria-liberta/slideshow/src/assets/slideshow.jpg',
   'https://images.unsplash.com/photo-1584226761916-3fd67ab5ac3a?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixlib=rb-1.2.1&q=80&w=500'
 ];
 
