e91b5370076401be69d65427eacd3bedada3088f
Gabriela Costa <gabrielasilvaalvescosta@gmail.com>
2022-01-11 02:23:27 -0300

    Dashboard (mobile e web pronto)

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Main, Dados, FundoProfile, FotoPerfil, Perfil, DadosPerfil, TituloPerfil, TopoPerfil, LinhaPerfil, ColunaPerfil, EditaPerfil } from '../components/Main/styles';
+import Loading from '../components/Loading/Loading';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
 
 function Dashboard() {
-  return (
+
+
+  return (    
     <div>
-      BEM VINDO.
+    <FundoProfile />
+   
+    <Perfil>
+    <FotoPerfil />
+    <DadosPerfil>
+      <TituloPerfil>Minha conta</TituloPerfil>
+      <h3>Gabriela Costa</h3>
+    </DadosPerfil>
+    </Perfil>
+   
+    <Dados>
+      <TopoPerfil>
+      <h2>Meus dados</h2>
+      <EditaPerfil>
+      <FontAwesomeIcon icon={faEdit} />
+      </EditaPerfil>
+      </TopoPerfil>
+
+      <LinhaPerfil>
+      <ColunaPerfil titulo>Endereço:</ColunaPerfil>
+      <ColunaPerfil>Rua Geraldina Carlos dos Santos, 67 - Núcleo Residencial Isabela - Taboão da Serra / SP
+      </ColunaPerfil>
+      </LinhaPerfil>
+
+
+      <LinhaPerfil>
+      <ColunaPerfil titulo>Email:</ColunaPerfil>
+      <ColunaPerfil>gabrielaaalvescosta@gmail.com</ColunaPerfil>
+      </LinhaPerfil>
+
+    
+      <LinhaPerfil>
+      <ColunaPerfil titulo>Senha</ColunaPerfil>
+      <ColunaPerfil>***********</ColunaPerfil>
+      </LinhaPerfil>
+
+
+      <LinhaPerfil>
+      <ColunaPerfil titulo>CPF</ColunaPerfil>
+      <ColunaPerfil>***********94</ColunaPerfil>
+      </LinhaPerfil>
+      
+    </Dados>
+
     </div>
   )
 }
