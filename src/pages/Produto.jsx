a0b8622a59fa94f353cc829de76b7f1e0f19585e
kasvrol <dovalekarolina@gmail.com>
2022-01-11 22:49:02 -0300

    consertos nas classes da pagina de produtos e adição dos estilos em css

--- src/pages/Produto.jsx
+++ src/pages/Produto.jsx
@@ -3,8 +3,8 @@ import { useParams } from 'react-router-dom';
 import CardLivro from '../components/CardLivro/CardLivro';
 import Loading from '../components/Loading/Loading';
 import { buscaLivroPeloId, buscaLivros } from '../services/livroApi';
-
 import Carrossel from '../components/Carrossel/Carrossel';
+import '../components/Livros/livros.css'
 
 function Produto() {
   const { idLivro } = useParams();  
@@ -38,13 +38,86 @@ function Produto() {
 
   if (!isLoaded) {
     return <Loading />;
-  } 
+  }
+
   return (
-    <div>     
-    {console.log(livro, livrosRelacionados)}
-    <h1>{livro.titulo}</h1>
-    <Carrossel livros={livrosRelacionados} />
-    </div>
+   <div className="body-produto">
+     <section>
+     <div className="imagem-cabecalho">
+          <img src={livro.url_img}/>
+      </div>
+      <div className="apresentacao-do-livro">
+            <div className="foto-do-livro">
+            <img src={livro.url_img}/>
+            </div>
+            <section>
+            <h6>{livro.categoria}</h6>
+            <h1>{livro.titulo}</h1>
+            <h2>{livro.preco}</h2>
+        </section>
+        <section className="informacoes-do-livro">
+              <h2>Informações do livro</h2>
+              <p>{livro.descricao}</p>
+        </section>
+        </div>
+            <div className="ficha-tecnica-do-livro">
+                <h2>
+                    Ficha técnica
+                </h2>
+                <div className="ficha-tecnica-do-livro-informacoes">
+                  <div>
+                    <h4>
+                      ISBN:
+                    </h4>
+                    <p>{livro.ISBN}</p>
+                  </div>
+                  <hr/>
+                  <div>
+                    <h4>
+                      Código de barras:
+                    </h4>
+                    <p>{livro.ISBN} - {livro.id_livro}</p>
+                  </div>
+                  <hr/>
+                  <div>
+                    <h4>
+                      Autor:
+                    </h4>
+                    <p>{livro.autor}</p>
+                  </div>
+                  <hr/>
+                  <div>
+                    <h4>
+                      Quantidade de Páginas:
+                    </h4>
+                    <p>{livro.paginas}</p>
+                  </div>
+                  <hr/>
+                  <div>
+                    <h4>
+                      Editora:
+                    </h4>
+                    <p>{livro.editora}</p>
+                  </div>
+                  <hr/>
+                  <div>
+                    <h4>
+                      Genero:
+                    </h4>
+                    <p>{livro.categoria}</p>
+                  </div>
+                </div>
+              </div>
+     </section>
+      
+        <section className="parte-do-carrossel">
+            <h4 className="titulo-relacionados">
+                Quem comprou esse, também viu:
+            </h4>
+            {console.log(livro, livrosRelacionados)}
+            <Carrossel livros={livrosRelacionados} />
+        </section>
+   </div>
     
   )
 }
